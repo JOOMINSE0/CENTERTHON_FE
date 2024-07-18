831a1f0d08fd70f0bf37b9e0cd1448acf3e878a9
joominse0 <sally51501512@gmail.com>
2024-07-18 15:18:30 +0900

    모달창

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react';
 import Board from "./pages/Board/board.js";
 import Createboard from "./pages/Board/createboard.js";
+import Modal from "./pages/Board/modal.js";
+import Modaldetail from "./pages/Board/modaldetail.js";
 import './App.css';
 
 function App() {
@@ -10,9 +12,8 @@ function App() {
       <Routes>
         <Route path="/board" element={<Board/>} /> {/* MZ 언어 게시판 */}
         <Route path="/createboard" element={<Createboard/>} /> {/* MZ 언어 작성 */}
-         {/* <Route path="/modal" element={<Modal/>} /> 모달창 */}
-        {/* <Route path="/recommend2" element={<Recommend2/>} /> 추천2 */}
-
+        <Route path="/modal" element={<Modal/>} /> {/* 작성하기 모달창 */}
+        <Route path="/modaldetail" element={<Modaldetail/>} /> {/* 언어 게시판 모달창 */}
       </Routes>
     </Router>
   );
--- src/pages/Board/Modal.js
+++ src/pages/Board/Modal.js
@@ -1,14 +1,22 @@
+import React from 'react';
 import './modal.css';
 
-function Modal(){
-    return(
-        <div className=''>
-
-            <div>
+function Modal({ closeModal }) {
+    return (
+        <div className='iphone-frameM'>
+            <div className='modal'>
+                <p>
+                    <img
+                        className='X'
+                        src='../../../img/X.png'
+                        alt='닫기'
+                        onClick={closeModal}
+                    />
+                </p>
                 <p>입력하신 MZ언어는 이미 등록되어</p>
                 <p>있습니다. 다른 MZ언어를 입력해주세요.</p>
+                <button className='checkBtn' onClick={closeModal}>확인</button>
             </div>
-
         </div>
     );
 }
