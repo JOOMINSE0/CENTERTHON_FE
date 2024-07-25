95c5ed73a32a2896f72a52c978fbd50e313445b5
gusdk45 <122820408+gusdk45@users.noreply.github.com>
2024-07-25 22:58:25 +0900

    Update translationrecord1.js

--- src/pages/Board/translationrecord1.js
+++ src/pages/Board/translationrecord1.js
@@ -9,7 +9,7 @@ function Translationrecord1() {
     const [currentPage, setCurrentPage] = useState(1);
     const [itemsPerPage] = useState(6);
     const [totalPages, setTotalPages] = useState(0);
-    const fetchURL = "http://ec2-3-34-152-209.ap-northeast-2.compute.amazonaws.com:8080/";
+    const fetchURL = "https://port-0-centerthon-be-lz124x0vc7996d99.sel4.cloudtype.app/";
 
     useEffect(() => {
         axios.get(fetchURL + 'api/history')
